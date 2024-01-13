17fe9bee2e6a7b17b55c56857efad5fd4c8661ed
msa-sadeghi <sadeghi.msai@gmail.com>
2024-01-13 20:00:10 +0330

    لودکردن تصاویر سفینه و مارتین # اضافه کردن فونت # پیدا کردن تصویر پس زمینه مناسب برای بازی # لود کردن تصویر پس زمینه در بازی # قراردادن سفینه در پایین صفحه #
    لودکردن تصاویر سفینه و مارتین
    # اضافه کردن فونت
    # پیدا کردن تصویر پس زمینه مناسب برای بازی
    # لود کردن تصویر پس زمینه در بازی
    # قراردادن سفینه در پایین صفحه
    #

---
+++ space/constants.py
@@ -0,0 +1,14 @@
+import pygame
+pygame.init()
+
+
+screen = pygame.display.set_mode()
+SCREEN_WIDTH = screen.get_width()
+SCREEN_HEIGHT = screen.get_height()
+
+#TODO  لودکردن تصاویر سفینه و مارتین
+# اضافه کردن فونت
+# پیدا کردن تصویر پس زمینه مناسب برای بازی
+# لود کردن تصویر پس زمینه در بازی
+# قراردادن سفینه در پایین صفحه
+# 
