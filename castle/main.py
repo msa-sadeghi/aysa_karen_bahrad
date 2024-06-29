2cd71a2b8e3a1f60af2d0a124a1078f493dcda08
msa-sadeghi <sadeghi.msai@gmail.com>
2024-06-29 20:57:59 +0330

    Update main.py

--- castle/main.py
+++ castle/main.py
@@ -7,13 +7,13 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 FPS = 60
-
+bg = pygame.image.load("assets/bg.png")
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             
-            
+    screen.blit(bg, (0,0))       
     pygame.display.update()
     clock.tick(FPS)
